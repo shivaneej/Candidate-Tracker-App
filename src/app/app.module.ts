7e516a1c3d8138bd9fac646143f21efcb5c70711
Shivanee <Shivaneej02@gmail.com>
2020-07-03 00:10:23 +0530

    Initial components setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CandidatesComponent } from './candidates/candidates.component';
+import { CandidateProfileComponent } from './candidate-profile/candidate-profile.component';
+import { InterviewFormComponent } from './interview-form/interview-form.component';
+import { UsersComponent } from './users/users.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { InterviewsComponent } from './interviews/interviews.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    ProfileComponent,
+    CandidatesComponent,
+    CandidateProfileComponent,
+    InterviewFormComponent,
+    UsersComponent,
+    SidebarComponent,
+    InterviewsComponent
   ],
   imports: [
     BrowserModule
