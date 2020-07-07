0e84ba21d0cfaf8990e8271838649b7d42fcffb4
Shivanee <Shivaneej02@gmail.com>
2020-07-08 02:22:00 +0530

    Separate users according to roles

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SystemUser } from '../models/system-user';
 import { UsersService } from '../services/users.service';
 import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import groupBy from '../helper';
 
 @Component({
   selector: 'app-users',
@@ -11,52 +11,35 @@ import { Observable } from 'rxjs';
 })
 export class UsersComponent implements OnInit {
 
-  // Temporary data
-  admins : SystemUser[] = [
-    { name: 'Admin 1', email: 'xyz@abc.com' }, 
-    { name: 'Admin 2', email: 'xyz@abc.com'} ];
-  ops : SystemUser[] = [
-    { name: 'OPS 1', email: 'xyz@abc.com' }, 
-    { name: 'OPS 2', email: 'xyz@abc.com'},
-    { name: 'OPS 3', email: 'xyz@abc.com' }];
-  
+  admins : SystemUser[];
+  ops : SystemUser[];
   recruiters : SystemUser[];
-  interviewers : SystemUser[] = [
-    { name: 'Int 1', email: 'xyz@abc.com'},
-    { name: 'Int 2', email: 'xyz@abc.com' }, 
-    { name: 'Int 3', email: 'xyz@abc.com'},
-    { name: 'Int 4', email: 'xyz@abc.com'},
-    { name: 'Int 5', email: 'xyz@abc.com'},
-    { name: 'Int 6', email: 'xyz@abc.com' }, 
-    { name: 'Int 7', email: 'xyz@abc.com'},
-    { name: 'Int 8', email: 'xyz@abc.com'},
-    { name: 'Int 9', email: 'xyz@abc.com'},
-    { name: 'Int 10', email: 'xyz@abc.com' }, 
-    { name: 'Int 11', email: 'xyz@abc.com'},
-    { name: 'Int 12', email: 'xyz@int.com'},
-    { name: 'Int 13', email: 'xyz@abc.com'},
-    { name: 'Int 14', email: 'xyz@abc.com' }, 
-    { name: 'Int 15', email: 'xyz@abc.com'},
-    { name: 'Int 16', email: 'xyz@abc.com'}
-  ];
-  columnHeader = {'name': 'Name', 'email': 'Email'};
-  recruiterColumnHeader = { 'name': 'Name', 'email': 'Email', 'contact' : 'Contact', 'manager' : 'Manager Email' };
-
+  interviewers : SystemUser[]; 
+  dataLoading : boolean = false;
+  columnHeader = { 'name': 'Name', 'email': 'Email', 'contact' : 'Contact' };
 
   constructor(private service : UsersService) { }
 
   ngOnInit(): void {
+    this.dataLoading = true;
     this.service.getAll().pipe(
-      map((recruiters : any) => {
-        return recruiters.map((rec) => {
-            let name = rec.firstName + " " + rec.lastName;
-            let email = rec.email || '';
-            let contact = rec.contact || '';
-            let manager = rec.manager || '';
-            return { name, email, contact, manager } as SystemUser;
+      map((users : any) => {
+        return users.map((user) => {
+            let name = user.firstName + " " + user.lastName;
+            let email = user.email;
+            let contact = user.contact || '';
+            let role = user.role.roleString;
+            return { name : name, email : email, contact : contact, role : role } as SystemUser;
         })
       }
     ))
-    .subscribe(recruiters => this.recruiters = recruiters);
+    .subscribe(users => {
+      let usersRole = groupBy(users, ( x: SystemUser ) => x.role);
+      this.admins = usersRole.Admin;
+      this.ops = usersRole.OPS;
+      this.recruiters = usersRole.Recruiter; 
+      this.interviewers = usersRole.Interviewer; 
+      this.dataLoading = false; 
+    });
   }
 }
