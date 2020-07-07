fa3ae996521125b9b52d4b9f11e5b1e24b3132d8
Harsh Patel <harsh.patel4@somaiya.edu>
2020-07-08 04:33:01 +0530

    Changed url endpoint in users service

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -9,7 +9,7 @@ export class UsersService extends DataService{
 
   constructor(http: HttpClient) { 
     // let url = 'http://localhost:3000/recruiters'; 
-    let url = 'http://localhost:8080/api/users'; 
+    let url = 'http://localhost:8080/users/'; 
     super(url, http);
   }
 }
