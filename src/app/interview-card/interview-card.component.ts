44cb8b73e08a8daf56415b4c02d73b60a6706968
Shivanee <Shivaneej02@gmail.com>
2020-07-13 00:35:06 +0530

    Changed date time format in interview cards

--- src/app/interview-card/interview-card.component.ts
+++ src/app/interview-card/interview-card.component.ts
@@ -11,9 +11,16 @@ export class InterviewCardComponent implements OnInit {
   @Input('interview') interview;
   status;
   statusColor;
+  date;
+  startTime;
+  endTime;
   constructor() { }
 
   ngOnInit(): void {
+    let dateObject = new Date(this.interview.start_time);
+    this.date = dateObject.toDateString();
+    this.startTime = dateObject.toLocaleString('en-US', { hour: '2-digit', minute: 'numeric', hour12: true });
+    this.endTime = new Date(this.interview.end_time).toLocaleString('en-US', { hour: '2-digit', minute: 'numeric', hour12: true });
     if(this.interview.recruiter_approved) {
       let intStatus = (this.interview.interviewer_approved) ? InterviewStatus.Confirmed : InterviewStatus.Pending;
       this.status = intStatus.message;
--- src/app/services/interview.service.ts
+++ src/app/services/interview.service.ts
@@ -17,16 +17,16 @@ export class InterviewService extends DataService {
   fetchInterviews() {
     return of([
       { interview_id : 1, candidate_id : 1, interviewer_id : 7,
-        date : "22/7/2020", start_time : 	"11:30 AM", end_time : "1:00 PM",
+        start_time : 	"2020-07-22 11:30:00", end_time : "2020-07-22 13:00:00",
         interviewer_approved : false, recruiter_approved : true },
       { interview_id : 2, candidate_id : 2, interviewer_id : 7,
-        date : "22/7/2020", start_time : 	"2:30 PM", end_time : "3:00 PM",
+        start_time : 	"2020-07-22 14:30:00", end_time : "2020-07-22 15:00:00",
         interviewer_approved : false, recruiter_approved : false },
       { interview_id : 3, candidate_id : 3, interviewer_id : 7,
-        date : "23/7/2020", start_time : 	"11:30 AM", end_time : "12:30 PM",
+        start_time : 	"2020-07-23 11:30:00", end_time : "2020-07-23 12:30:00",
         interviewer_approved : true, recruiter_approved : true },
       { interview_id : 4, candidate_id : 4, interviewer_id : 7,
-        date : "23/7/2020", start_time : 	"1:00 PM", end_time : "1:45 PM",
+        start_time : 	"2020-07-23 13:00:00", end_time : "2020-07-23 13:45:00",
         interviewer_approved : true, recruiter_approved : true },
     ]);
   }
